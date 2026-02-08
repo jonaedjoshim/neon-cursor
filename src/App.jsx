e8966fa64ae49e6313429a83afd5fc0e92082079
mdjonaedjoshim <mdjonaedjoshim@gmail.com>
2026-02-08 14:03:58 +0600

    created particles

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,19 @@ function App() {
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
 
+    let particles = []
+
+    class Particle {
+      constructor(x, y) {
+        this.x = x
+        this.y = y
+        this.size = Math.random() * 5 + 1
+        this.speedX = Math.random() * 3 - 1.5
+        this.speedY = Math.random() * 3 - 1.5
+        this.color = `hsl(${Math.random() * 360}, 50%, 50%)`
+      }
+    }
+
     const handleMouseMove = (e) => {
       const x = e.clientX
       const y = e.clientY
