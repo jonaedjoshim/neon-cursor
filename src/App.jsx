2bf9a86695805f95d5a65f1b03a9bb9821ce871c
mdjonaedjoshim <mdjonaedjoshim@gmail.com>
2026-02-08 12:56:08 +0600

    neon-cursor

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='bg-black h-screen m-0 p-0 font-mono flex justify-center select-none text-2xl text-gray-400 overflow-hidden'>
+      <h2 className='lg:pt-8 pt-5'>Hover Your Mouse for Neon Effect</h2>
+    </div>
+  )
+}
+
+export default App
